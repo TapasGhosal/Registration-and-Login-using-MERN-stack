3a53dcf004c3c846d0a7dccdeb9d7673df1343e2
Ghosal <tghosal@its.jnj.com>
2021-03-19 18:19:02 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch, Link, useRouteMatch, useParams } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch, Link } from "react-router-dom";
 
 import Login from './Login';
 import Registration from './Registration';
